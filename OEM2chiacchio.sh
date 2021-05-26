7487faa35753510bdab3c9d09acfe7bdf4215fea
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2021-05-26 15:38:07 +0200

    Update OEM2chiacchio.sh

--- OEM2chiacchio.sh
+++ OEM2chiacchio.sh
@@ -19,20 +19,62 @@ if [ ! -f "/etc/debian_version" ]; then
 fi
 }
 
+function installLibDrm {
+	cd /tmp
+	mkdir lidrm
+	cd libdrm
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-amdgpu1_2.4.105-1-francy_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-common_2.4.105-1-francy_all.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-etnaviv1_2.4.105-1-francy_arm64.deb	
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-freedreno1_2.4.105-1-francy_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-libkms_2.4.105-1-francy_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-nouveau2_2.4.105-1-francy_arm64.deb	
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-radeon1_2.4.105-1-francy_arm64.deb 
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-tegra0_2.4.105-1-francy_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-tests_2.4.105-1-francy_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm2_2.4.105-1-francy_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/2.4.105-francy/libdrm-dev_2.4.105-1-francy_arm64.deb
+	dpkg -i *.deb
+	cd ..
+	rm -rf libdrm 
+	apt-get -f install
+}
+
+function InstallLibMesa {
+	cd /tmp
+	mkidr mesa
+	cd mesa
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libd3dadapter9-mesa_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libegl-mesa0_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libegl1-mesa_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libegl1_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libgbm1_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libgl1-mesa-dri_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libgl1-mesa-glx_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libgl1_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libglapi-mesa_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libgles2-mesa_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libgles2_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libglx-mesa0_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/libosmesa6_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/mesa-opencl-icd_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/mesa-va-drivers_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/mesa-vdpau-drivers_20.3.5-1-chiacchio_arm64.deb
+	wget https://github.com/numerunix/piccolinux/releases/download/20.3.5-chiacchio/mesa-vulkan-drivers_20.3.5-1-chiacchio_arm64.deb
+	dpkg -r --force-depends libglvnd0 libglx0
+	dpkg -i *.deb
+	cd ..
+	rm -rf mesa
+	apt-get -f install
+} 
 notRoot
 checkSystem
 instalPrerequisites
 
-dialog --title "Informazioni" --backtitle "Informazioni" --msgbox "Adesso disabilito il blocco degli aggiornamenti, accertarsi che il repository della numeronesoft sia attivo." 40 60
 
-
-apt-mark unhold libegl-mesa0 libegl1 libgbm1 libgl1-mesa-dri libgl1 libglapi-mesa libgles2 libglx-mesa0 mesa-va-drivers mesa-vdpau-drivers libdrm-amdgpu1 libdrm-common libdrm-nouveau2 libdrm-radeon1 libdrm2
-
-dialog --title "Informazioni" --backtitle "Informazioni" --msgbox "Eseguo tutti gli aggionrnameni, anche delle libgl" 40 60
-
-
-apt update
-apt upgrade -y
-apt install libegl1 libgl1 libgles2 -y
+dialog --title "Informazioni" --backtitle "Informazioni" --msgbox "Eseguo le operaazioni, rimuovendo libglvnd0 e liglx0" 40 60
+InstallLibDrm
+InstallLibMesa
+apt-get -f install
 
 dialog --title "Informazioni" --backtitle "Informazioni" --msgbox "Copyright 2021 Gulio Sorrentino\nConcesso in licenza secondo la GPL v3 o, secondo la tua opinione, qualsiasi versione successiva.\nIlsoftware viene concesso per come è, senza nessuna garanzia, né implicita né esplicita.\nSe ti è sembrato interessante considera una donazione su paypal.\nDedicato a Francesca dei centri sociali (san severino? milano? salerno?)" 40 60
