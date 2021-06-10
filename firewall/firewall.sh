1753ce6b6d945539d2cb62d7e38f1df06d283720
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2021-06-10 14:26:58 +0200

    si può richiamare come servizio
    ricordatevi di mettere le vostre periferiche, che ottenete con ip addr. questo è statico e si può richiamare come servizio.

---
+++ firewall/firewall.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#Autore Giulio Sorentino <gsorre84@gmail.com>
+#Concesso in licenza secondo la GPL V3
+
+iptables -A INPUT -i enp31s0 -j DROP
+iptables -A FORWARD -i enp31s0 -j DROP
+iptables -A INPUT -i wlp37s0 -j DROP
+iptables -A FORWARD -i wlp37s0 -j DROP
+
+#for i in `ls /sys/class/net`; do 
+#if [[ $i != "lo" ]]; then
+# iptables -A INPUT -i $i -j DROP
+# iptables -A FORWARD -i $i -j DROP
+#fi
+#done
