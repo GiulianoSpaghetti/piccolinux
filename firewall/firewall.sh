3be19daec8c7d35789b3d9b41e42edba7af440e4
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2021-06-10 07:32:10 +0200

    francy edition

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -2,11 +2,6 @@
 #Autore Giulio Sorentino <gsorre84@gmail.com>
 #Concesso in licenza secondo la GPL V3
 
-#iptables -A INPUT -i enp31s0 -j DROP
-#iptables -A FORWARD -i enp31s0 -j DROP
-#iptables -A INPUT -i wlp37s0 -j DROP
-#iptables -A FORWARD -i wlp37s0 -j DROP
-
 for i in `ls /sys/class/net`; do 
 if [[ $i != "lo" ]]; then
  iptables -A INPUT -i $i -j DROP
