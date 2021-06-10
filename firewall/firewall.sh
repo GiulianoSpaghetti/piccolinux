76f2d8b26c7d8ef2bc466dcbbc4987cd32c89df4
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2021-06-10 07:31:36 +0200

    Add files via upload

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+﻿#!/bin/sh
 #Autore Giulio Sorentino <gsorre84@gmail.com>
-iptables -A INPUT -i enp31s0 -j DROP
-iptables -A FORWARD -i enp31s0 -j DROP
-iptables -A INPUT -i wlp37s0 -j DROP
-iptables -A FORWARD -i wlp37s0 -j DROP
+#Concesso in licenza secondo la GPL V3
 
+#iptables -A INPUT -i enp31s0 -j DROP
+#iptables -A FORWARD -i enp31s0 -j DROP
+#iptables -A INPUT -i wlp37s0 -j DROP
+#iptables -A FORWARD -i wlp37s0 -j DROP
+
+for i in `ls /sys/class/net`; do 
+if [[ $i != "lo" ]]; then
+ iptables -A INPUT -i $i -j DROP
+ iptables -A FORWARD -i $i -j DROP
+fi
+done
