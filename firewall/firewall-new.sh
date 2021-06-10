4e381a0f3df04956ae9e5f5e5fccdcc72a69b2ad
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2021-06-10 14:27:53 +0200

    Update firewall-new.sh

--- firewall/firewall-new.sh
+++ firewall/firewall-new.sh
@@ -1,6 +1,7 @@
 ﻿#!/bin/sh
 #Autore Giulio Sorentino <gsorre84@gmail.com>
 #Concesso in licenza secondo la GPL V3
+#dediato a franceesca salerno
 
 for i in `ls /sys/class/net`; do 
 if [[ $i != "lo" ]]; then
