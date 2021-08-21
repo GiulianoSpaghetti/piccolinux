16b25124b82dcb75360655cb78f004ee3ff8484b
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2021-08-21 21:00:03 +0200

    Update project_milano1.sh

--- project_milano1.sh
+++ project_milano1.sh
@@ -611,7 +611,7 @@ case $init in
 	initstr=runit-init
 	#apt-get install --reinstall --purge $(dpkg --get-selections | grep -w 'install$' | cut -f1) $initstr -y
 	mv /sbin/start-stop-daemon.REAL /sbin/start-stop-daemon
-        apt-mark hold runit-init runit-sysv
+        apt-mark hold runit-init
         echo "runit-init" | sudo dpkg --set-selections
 
 ;;
