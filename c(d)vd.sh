a337c0c6483a529c49a635c7082e0b8743da00f6
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-03-25 23:20:42 +0100

    Update c(d)vd.sh

--- c(d)vd.sh
+++ c(d)vd.sh
@@ -135,6 +135,6 @@ dialog --title "Informazione" \
 	i=`expr $i + 1`	
 done
 
-dialog	--msgbox "Copyright 2021 Giulio Sorrentino <gsorre84@gmail.com>\nIl software viene concesso in licenza secondo la GPL v3 o, secondo la tua opionione, qualsiasi versione successiva.\nIl software viene concesso per COME E', senza NESSUNA GARANZIA ne' implicita ne' esplicita.\nSe ti piace, considera una donazione tramite paypal.\nDedicato a Annachiara Milano.\nHappy Hacking :)" 40 60>/dev/tty
+dialog	--msgbox "Copyright 2021 Giulio Sorrentino <gsorre84@gmail.com>\nIl software viene concesso in licenza secondo la GPL v3 o, secondo la tua opionione, qualsiasi versione successiva.\nIl software viene concesso per COME E', senza NESSUNA GARANZIA ne' implicita ne' esplicita.\nSe ti piace, considera una donazione tramite paypal.\nHappy Hacking :)" 40 60>/dev/tty
 
 
