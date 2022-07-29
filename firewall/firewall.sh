e3cbab720bfe63a58f4e83ccc7a5db87b9832033
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-07-29 06:49:43 +0200

    Update firewall.sh

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -6,7 +6,8 @@ iptables -A INPUT -i enp31s0 -j DROP
 iptables -A FORWARD -i enp31s0 -j DROP
 iptables -A INPUT -i wlp37s0 -j DROP
 iptables -A FORWARD -i wlp37s0 -j DROP
-
+iptables -A INPUT -p udp --dport 53 -j DROP
+iptables -A FORWARD -p udp --dport 53 -j DROP
 #for i in `ls /sys/class/net`; do 
 #if [[ $i != "lo" ]]; then
 # iptables -A INPUT -i $i -j DROP
