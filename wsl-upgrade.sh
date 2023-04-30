79e419d629ab320812ff39816c44056cdfa95ef7
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2023-04-30 09:08:28 +0200

    Update and rename wsl.sh.txt to wsl-upgrade.sh

--- wsl-upgrade.sh
+++ wsl-upgrade.sh
@@ -1,6 +1,12 @@
 # Copyrigth 2023 Giulio Sorrentino, Some Right Reserved
+# original source: https://gist.github.com/charlie-x/96a92aaaa04346bdf1fb4c3621f3e392#file-gistfile1-txt-L31
 #!/bin/bash
 sudo apt install build-essential flex bison dwarves libssl-dev libelf-dev bc git
+
+if [ $# -ne 2 ]; then
+echo "Bisogna passare due parametri: il primo il numero di processori da utilizzare, il secondo il nome della propria home directory di windows. Il programma termina."
+exit
+fi
 
 git clone https://github.com/microsoft/WSL2-Linux-Kernel.git
 
