91533da2a3b6f4950fb4b45702cced8f46c8545b
Giulio Sorrentino <numerone@fastwebnet.it>
2024-03-21 19:55:24 +0000

    Update piccolinux-napoli-edition.sh

--- piccolinux-napoli-edition.sh
+++ piccolinux-napoli-edition.sh
@@ -28,8 +28,8 @@ if [ $? -eq 0 ]; then
  fi
 
 sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/numeronesoft.gpg --keyserver keyserver.ubuntu.com --recv-keys 37A40DBA52B68EEB
-echo "deb [signed-by=/usr/share/keyrings/numeronesoft.gpg] http://numeronesoft.ddns.net $repo main
-deb-src [signed-by=/usr/share/keyrings/numeronesoft.gpg] http://numeronesoft.ddns.net $repo main" | sudo tee /etc/apt/sources.list.d/numeronesoft.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/numeronesoft.gpg] http://numeronesoft.ddns.net:8080/apt/debian $repo main
+deb-src [signed-by=/usr/share/keyrings/numeronesoft.gpg] http://numeronesoft.ddns.net:8080/apt/debian $repo main" | sudo tee /etc/apt/sources.list.d/numeronesoft.list > /dev/null
 return 1
 }
 
